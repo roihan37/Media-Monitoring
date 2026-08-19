fdd40da57a318724e7d0465d6aa0a3327335d4eb
Amir Amilin <amiramilin@MacBook-Pro-Amir.local>
2026-08-19 15:17:29 +0700

    feat: implement mention deduplication

--- backend/utils/duplicate-detector.ts
+++ backend/utils/duplicate-detector.ts
@@ -1,61 +1,90 @@
 import { Mention } from "../models/mention.model";
 
 export function deduplicateMentions(
-    mentions : Mention[]
+    mentions: Mention[]
 ) {
     const unique = new Map<string, Mention>()
 
-    for (const mention of mentions){
+    for (const mention of mentions) {
         const key = getDuplicateKey(mention);
+        // key return url: .... or fallback | .....
+
+        const existing = unique.get(key)
+
+        // if no duplicate
+        if (!existing) {
+            unique.set(key, mention);
+            continue;
+        }
+
+        // IF DUPLICATE
+        // Take the highest engagement
+        if (mention.engagement > existing.engagement) {
+            existing.engagement = mention.engagement;
+        }
+
+        if (!existing.author && mention.author) {
+            existing.author = mention.author;
+        }
+
+        if (!existing.title && mention.title) {
+            existing.title = mention.title;
+        }
+
+        if (!existing.content && mention.content) {
+            existing.content = mention.content;
+        }
     }
+
+    return Array.from(unique.values());
 }
 
 // Handle duplicate
 function getDuplicateKey(
-    mention : Mention
-) : string {
+    mention: Mention
+): string {
     const url = normalizeUrlForComparison(mention.url);
 
     // main URL
     if (url) {
-    return `url:${url}`;
+        return `url:${url}`;
     }
 
     // example date format : 2026-08-19
     const date = mention.publishedAt
-    ? mention.publishedAt.toISOString().slice(0, 10) 
-    : ""
+        ? mention.publishedAt.toISOString().slice(0, 10)
+        : ""
 
     return [
-    "fallback",
-    mention.sourceKey,
-    normalizeTitleForComparison(mention.title),
-    date,
-  ].join("|");
+        "fallback",
+        mention.sourceKey,
+        normalizeTitleForComparison(mention.title),
+        date,
+    ].join("|");
 
 }
 
 // normalize URL for Comparison
 function normalizeUrlForComparison(
-    url : string | null
-): string | null{
-    if(!url){
+    url: string | null
+): string | null {
+    if (!url) {
         return null
     }
 
     // example : https://example.com/article
     return url
-    .trim()
-    .replace(/\/+$/, "")
-    .toLowerCase();
+        .trim()
+        .replace(/\/+$/, "")
+        .toLowerCase();
 }
 
 // normalize title for Comparison
 function normalizeTitleForComparison(
-  title: string | null
+    title: string | null
 ): string {
-  return (title ?? "")
-    .trim()
-    .toLowerCase()
-    .replace(/\s+/g, " ");
+    return (title ?? "")
+        .trim()
+        .toLowerCase()
+        .replace(/\s+/g, " ");
 }
